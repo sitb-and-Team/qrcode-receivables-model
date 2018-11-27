69cc5f699ee81040524e70c8eec8fb28d4c71556
yangyao <873241789@qq.com>
2018-11-27 21:41:27 +0800

    del 多余配置

--- src/index.html.js
+++ src/index.html.js
@@ -19,26 +19,6 @@ module.exports = function ({htmlWebpackPlugin}) {
   <meta id="eqMobileViewport" name="viewport"
         content="width=320, initial-scale=1, maximum-scale=1, user-scalable=no"/>
   <script type="text/javascript">
-    // 媒体查询
-    (function (doc, win) {
-      let docEl = doc.documentElement,
-        docTitle = doc.getElementById('htmlTitle');
-      // orientationchange 事件是在用户水平或者垂直翻转设备（即方向发生变化）时触发的事件。
-      // onresize 事件会在窗口或框架被调整大小时发生。
-      resizeEvt = "orientationchange" in window ? "orientationchange" : "resize",
-        recalc = function () {
-          window.clientWidth = docEl.clientWidth;
-          docTitle.innerHTML = "${htmlWebpackPlugin.options.title}";
-          if (!window.clientWidth) return;
-          // 大于移动端最大尺寸，就使用原始大小
-          if (window.clientWidth > 414) return;
-          docEl.style.fontSize = 40 * (window.clientWidth / 640) + "px";
-          window.base = 40 * (window.clientWidth / 640);
-        };
-      if (!doc.addEventListener) return;
-      win.addEventListener(resizeEvt, recalc, false);
-      doc.addEventListener("DOMContentLoaded", recalc, false);
-    })(document, window);
     window.config = {
       api: '${api}'
     };
