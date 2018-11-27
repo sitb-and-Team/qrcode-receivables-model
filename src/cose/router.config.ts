07f07176a48b94280f6e66720ab6071ef2a0e234
yangyao <873241789@qq.com>
2018-11-27 16:53:53 +0800

    配置路由

--- src/cose/router.config.ts
+++ src/cose/router.config.ts
@@ -3,30 +3,35 @@
  * author: yangyao(873241789@qq.com)
  * date: 2018/8/8
  */
-import { Home } from '../container/Home';
+import { Receivables } from '../container/Receivables';
 import { SuccessPage } from '../container/SuccessPage';
 import { ErrorPage } from '../container/ErrorPage';
+import { menu } from '../constants/zh-cn';
 
 export interface RouterConfig {
   path: string;
-  component: any
+  component: any;
+  title: string;
 }
 
 // 路由path
 export const routerPath = {
-  home: '/home',
+  receivables: '/receivables',
   successPage: '/successPage',
   errorPage: '/errorPage'
 };
 
 // 路由基本配置
 export const routerConfig: Array<RouterConfig> = [{
-  path: routerPath.home,
-  component: Home
+  title: menu.receivables,
+  path: routerPath.receivables,
+  component: Receivables
 }, {
+  title: menu.receivablesDetailed,
   path: routerPath.successPage,
   component: SuccessPage
 }, {
+  title: menu.receivablesDetailed,
   path: routerPath.errorPage,
   component: ErrorPage
 }];
